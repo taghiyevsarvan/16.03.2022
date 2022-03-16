518e1ac0653f90768f967b66dd04ba78ed0c0253
taghiyevsarvan <tu54wdlsg@code.edu.az>
2022-03-16 11:20:28 +0400

    Proje dosyası ekle.

---
+++ 16.03.2022/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _16._03._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "";
+            string password;
+            User user = new User(username);
+            do
+            {
+                Console.WriteLine("***************************************");
+                Console.Write("User Name daxil edin: ");
+                username = Console.ReadLine();
+                user.UserName = username;
+                if (user.UserName == null)
+                    Console.WriteLine("User Name uzunluqu 6 ve 25 arasi olmalidir: ");
+            } while (user.UserName == null);
+            do
+            {
+                Console.WriteLine("***************************************");
+                Console.WriteLine("Sifrede boyuk,kicik herf ve Reqem olmalidir....");
+                Console.Write("Sifreni Daxil edin: ");
+                
+                password = Console.ReadLine();
+                user.Password = password;
+                if (user.Password == null)
+                    Console.WriteLine("Pasword uzunluqu 6 ve 25 arasi olmalidir(boyuk,kicik herf ve en azn bir reqem olmalidir): ");
+            } while (user.Password == null);
+        }
+    }
+}
---
+++ 16.03.2022/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16._03._2022
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        public User(string username)
+        {
+            UserName = username;
+        }
+        public string UserName
+        {
+            get
+            {
+                return this._username;
+            }
+            set
+            {
+                if (value.Length > 5 && value.Length < 26 && !string.IsNullOrWhiteSpace(value))
+                    this._username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                if (value.Length > 7 && value.Length < 26 && HasDigit(value) && HasLower(value) && HasUpper(value) && !string.IsNullOrWhiteSpace(value))
+                    this._password = value;
+            }
+        }
+        static bool HasDigit(string pasword)
+        {
+            for (int i = 0; i < pasword.Length; i++)
+            {
+                if (char.IsDigit(pasword[i]))
+                    return true;
+            }
+
+            return false;
+        }
+
+        static bool HasLower(string pasword)
+        {
+            for (int i = 0; i < pasword.Length; i++)
+            {
+                if (char.IsLower(pasword[i]))
+                    return true;
+            }
+
+            return false;
+        }
+
+        static bool HasUpper(string pasword)
+        {
+            for (int i = 0; i < pasword.Length; i++)
+            {
+                if (char.IsUpper(pasword[i]))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+}
